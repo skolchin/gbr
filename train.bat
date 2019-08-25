5df4a43ce1c50606c892ce7469ede4db95470bda
skolchin <skolchin@gmail.com>
2019-08-25 20:13:11 +0300

    Regenerated

--- train.bat
+++ train.bat
@@ -1,7 +1,7 @@
 @echo off
 set PYTHONPATH=%PYTHONPATH%;.
 if "%1%" == "log" (
-%FASTER_RCNN_HOME%\tools\train_net.py --solver .\models\solver.prototxt --imdb gbr_train --iter 10000 --cfg .\models\gbr_rcnn.yml 1> .\logs\train.log 2>&1
+%FASTER_RCNN_HOME%\tools\train_net.py --solver .\models\solver.prototxt --imdb gbr_train --iter 20000 --cfg .\models\gbr_rcnn.yml 1> .\logs\train.log 2>&1
 ) else (
-%FASTER_RCNN_HOME%\tools\train_net.py --solver .\models\solver.prototxt --imdb gbr_train --iter 10000 --cfg .\models\gbr_rcnn.yml
+%FASTER_RCNN_HOME%\tools\train_net.py --solver .\models\solver.prototxt --imdb gbr_train --iter 20000 --cfg .\models\gbr_rcnn.yml
 )
