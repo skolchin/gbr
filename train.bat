827e57b24b8513bf162453d82699df2449afea2d
skolchin <skolchin@gmail.com>
2019-09-03 23:45:42 +0300

    updated

--- train.bat
+++ train.bat
@@ -3,7 +3,13 @@
 set PYTHONPATH=%PYTHONPATH%;.
 where wtee.exe > nul
 if %ERRORLEVEL% EQU 0 (
-%FASTER_RCNN_HOME%\tools\train_net.py --solver .\models\solver.prototxt --imdb gbr_train --iter 20000 --cfg .\models\gbr_rcnn.yml 2>&1 | wtee out\logs\train.log
+echo ***
+echo *** Saving log to out\logs\train.log
+echo ***
+%FASTER_RCNN_HOME%\tools\train_net.py --solver .\models\solver.prototxt --imdb gbr_train --iter 10000 --cfg .\models\gbr_rcnn.yml 2>&1 | wtee out\logs\train.log
 ) else (
-%FASTER_RCNN_HOME%\tools\train_net.py --solver .\models\solver.prototxt --imdb gbr_train --iter 20000 --cfg .\models\gbr_rcnn.yml
+echo ***
+echo *** Logging to console
+echo ***
+%FASTER_RCNN_HOME%\tools\train_net.py --solver .\models\solver.prototxt --imdb gbr_train --iter 10000 --cfg .\models\gbr_rcnn.yml
 )
