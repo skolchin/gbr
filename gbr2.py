5b833f5f9d52b78520febfdc546feaa5cb718503
skolchin <skolchin@gmail.com>
2019-11-18 00:19:27 +0300

    multiple changes

--- gbr2.py
+++ gbr2.py
@@ -17,7 +17,7 @@ from gr.utils import format_stone_pos
 
 import numpy as np
 import cv2
-import sys
+import os
 from PIL import Image, ImageTk
 import logging
 
@@ -36,8 +36,12 @@ class GbrOptionsDlg(tk.Toplevel):
             self.attributes("-toolwindow", True)
             self.title("Parameters")
 
-            x = parent.root.winfo_x() + parent.root.winfo_width()
-            y = parent.root.winfo_y() + 50
+            self.board_size_label = None
+            self.board_size_scale = None
+            self.board_size_disabled = None
+
+            x = parent.root.winfo_x() + parent.root.winfo_width() + 15
+            y = parent.root.winfo_y() + 40
             self.geometry("%+d%+d" % (x, y))
 
             internalFrame = tk.Frame(self)
@@ -47,7 +51,9 @@ class GbrOptionsDlg(tk.Toplevel):
             buttonFrame = tk.Frame(self, bd = 1, relief = tk.RAISED)
             buttonFrame.pack(side = tk.BOTTOM, fill = tk.X)
 
+            self.btn_image = ImgButton.get_ui_image("detect_flat.png")
             tk.Button(buttonFrame, text = "Detect",
+                image = self.btn_image, compound="left",
                 command = self.detect_callback).pack(side = tk.LEFT, padx = 5, pady = 5)
 
             tk.Button(buttonFrame, text = "Close",
@@ -60,11 +66,25 @@ class GbrOptionsDlg(tk.Toplevel):
             p = dict()
             for key in self.tkVars.keys():
                 p[key] = self.tkVars[key].get()
+
+            if self.board_size_disabled.get() > 0:
+                del p['BOARD_SIZE']
+
             self.parent.board.params = p
             self.parent.detect_stones()
 
         def close_callback(self):
+            self.parent.buttons['params'].state = False
             self.destroy()
+
+        def scale_cb_changed(self):
+            if self.board_size_disabled is not None and \
+                self.board_size_label is not None and \
+                self.board_size_scale is not None:
+
+                state = tk.DISABLED if self.board_size_disabled.get() > 0 else tk.NORMAL
+                self.board_size_label.config(state = state)
+                self.board_size_scale.config(state = state)
 
         # Add Scale widgets with board recognition parameters
         def add_switches(self, rootFrame, params, max_in_row = 6):
@@ -98,7 +118,7 @@ class GbrOptionsDlg(tk.Toplevel):
                         n = 0
                         ncol = ncol + 1
 
-                    # Add a switch
+                    # Add a scale from properties
                     caption = GR_PARAMS_PROP[key][3] if len(GR_PARAMS_PROP[key]) > 3 else key
                     label = tk.Label(frame, text = caption)
                     label.grid(row = n, column = 0, padx = 2, pady = 0, sticky = "s", ipady=4)
@@ -111,8 +131,27 @@ class GbrOptionsDlg(tk.Toplevel):
                                             variable = v)
                     scale.grid(row = n, column = 1, padx = 2, pady = 0)
                     vars[key] = v
-
                     n = n + 1
+
+                    # For board_size, add additional checkbox
+                    if key == 'BOARD_SIZE':
+                        self.board_size_label = label
+                        self.board_size_scale = scale
+
+                        state = tk.DISABLED if params[key] is None else tk.NORMAL
+                        label.config(state = state)
+                        scale.config(state = state)
+
+                        self.board_size_disabled = tk.IntVar()
+                        self.board_size_disabled.set(1 if state == tk.DISABLED else 0)
+                        cb = tk.Checkbutton(frame,
+                                            text = "Automatically detect board size",
+                                            variable = self.board_size_disabled,
+                                            command = self.scale_cb_changed)
+                        #if state == tk.NORMAL: cb.select()
+                        cb.grid(row = n, columnspan = 2, padx = 2, pady = 0)
+                        n = n + 1
+
             return vars
 
 
@@ -125,14 +164,15 @@ class GbrGUI2(object):
         self.buttons = dict()
         self.board = GrBoard()
         self.binder = NBinder()
+        self.optionsDlg = None
 
         self.internalFrame = tk.Frame(self.root)
         self.internalFrame.pack(fill = tk.BOTH, expand = True)
 
         self.__init_menu()
         self.__init_toolbar()
-        self.__init_window()
         self.__init_statusbar()
+        self.__init_window()
 
     # Initialization functions
     def __init_menu(self):
@@ -232,8 +272,12 @@ class GbrGUI2(object):
 
     def set_params_callback(self, event, tag, state):
         """Detection params button click"""
-        self.optionsDlg = GbrOptionsDlg(self)
-        return False
+        if self.optionsDlg is not None:
+            self.optionsDlg.destroy()
+            self.optionsDlg = None
+        if state:
+            self.optionsDlg = GbrOptionsDlg(self)
+        return True
 
     def detect_callback(self, event, tag, state):
         """Detect button click"""
@@ -285,73 +329,66 @@ class GbrGUI2(object):
     # Core functions
     #
     def load_image(self, filename):
+
+        # Clean up
         GrLog.clear()
-        try:
-            # Clean up
-            self.buttons['area'].state = False
-            self.imageMask.hide()
+        self.buttons['area'].state = False
+        self.buttons['params'].state = False
 
-            # Load image
-            self.board.load_image(filename, f_process = False, f_with_params = True)
+        self.imageMask.hide()
+        if self.optionsDlg is not None:
+            self.optionsDlg.destroy()
+            self.optionsDlg = None
 
-            # Display loaded image and mask
-            self.imagePanel.set_image(self.board.image)
-            self.imageMask.scaled_mask = self.board.param_board_edges
+        # Load image
+        self.board.load_image(filename, f_process = False, f_with_params = True)
 
-            # Reset button states
-            self.buttons['reset'].disabled = not self.board.can_reset_image
-            for b in ["edge", "area", "params", "detect"]:
-                self.buttons[b].disabled = False
+        # Display loaded image and mask
+        self.imagePanel.set_image(self.board.image)
+        self.imageMask.scaled_mask = self.board.param_board_edges
 
-            # Update status
-            if GrLog.numErrors() > 0:
-                self.statusBar.set("Errors during file loading, click here for the log")
-            else:
-                self.statusBar.set_file("File loaded", self.board.image_file)
+        # Reset button states
+        self.buttons['reset'].disabled = not self.board.can_reset_image
+        for b in ["edge", "area", "params", "detect"]:
+            self.buttons[b].disabled = False
 
-        except:
-            logging.exception("Error")
+        # Update status
+        if GrLog.numErrors() > 0:
             self.statusBar.set("Errors during file loading, click here for the log")
+        else:
+            self.statusBar.set_file("File loaded", self.board.image_file)
+
 
     def detect_edges(self, f_force = False):
         if not self.board.param_board_edges is None and not f_force:
            return
 
+        # Process
         GrLog.clear()
-        try:
-            self.board.detect_edges()
-            if GrLog.numErrors() > 0:
-               self.statusBar.set("Automatic board detection failed, click here for the log")
-            else:
-                self.statusBar.set("Board size detected as {s}x{s}".format(
-                                        s = self.board.board_size))
-                self.imageMask.scaled_mask = self.board.param_board_edges
-                self.imageMask.size = self.board.board_size
+        self.board.detect_edges()
 
-        except:
-            logging.exception("Error")
-            self.statusBar.set("Errors during grid detection, click here for the log")
-
-
+        if GrLog.numErrors() > 0:
+           self.statusBar.set("Automatic board detection failed, click here for the log")
+        else:
+            self.statusBar.set("Board size detected as {s}x{s}".format(
+                                    s = self.board.board_size))
+            self.imageMask.scaled_mask = self.board.param_board_edges
+            self.imageMask.size = self.board.board_size
 
     def detect_stones(self):
+        # Process
         GrLog.clear()
-        try:
-            # Process
-            self.board.process()
+        self.board.process()
 
-            # Update status
-            if GrLog.numErrors() > 0:
-                self.statusBar.set("Errors during processing, click here for the log")
-            else:
-                self.statusBar.set("{b} black, {w} white stones on {s}x{s} board detected".format(
-                    b = len(self.board.black_stones),
-                    w = len(self.board.white_stones),
-                    s = self.board.board_size))
-
-        except:
-            logging.exception("Error")
+        # Update status
+        if GrLog.numErrors() > 0:
             self.statusBar.set("Errors during processing, click here for the log")
+        else:
+            self.statusBar.set("{b} black, {w} white stones on {s}x{s} board detected, click here for the log".format(
+                b = len(self.board.black_stones),
+                w = len(self.board.white_stones),
+                s = self.board.board_size))
+
 
     #
     # Utility functions
