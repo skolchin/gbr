d40307c748c851794cbfb4cedc1b74a2f0b29dd8
skolchin <skolchin@gmail.com>
2019-12-20 11:48:53 +0300

    experiments

---
+++ cc/n/train.bat
@@ -0,0 +1,1 @@
+C:\Users\kol\Downloads\opencv\build\x64\vc15\bin\opencv_traincascade -data m/ -vec samples.vec -bg negatives.txt -numPos 300 -numNeg 300 -nstages 20 -nsplits 2 -minhitrate 0.999 -maxfalsealarm 0.5 -mem 512 -mode ALL -numThreads 10
--- cc/train.bat
+++ cc/train.bat
@@ -1,2 +1,1 @@
-cd n
-C:\Users\kol\Downloads\opencv\build\x64\vc15\bin\opencv_traincascade -data ./../m -vec ./../samples.vec -bg negatives.txt -numPos 300 -numNeg 300
+C:\Users\kol\Downloads\opencv\build\x64\vc15\bin\opencv_traincascade -data m/ -vec samples.vec -bg negatives.txt -numPos 300 -numNeg 300
