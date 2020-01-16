4ddbb6cb7adee48c09b607a7a99cabdd9fd35fcb
skolchin <skolchin@gmail.com>
2020-01-16 18:03:15 +0300

    experiments with TF

--- tf/item_classifier.py
+++ tf/item_classifier.py
@@ -19,18 +19,19 @@ from random import randrange
 
 IMG_HEIGHT = 20
 IMG_WIDTH = 20
-NUM_EPOCHS = 15
+NUM_EPOCHS = 20
 BATCH_SIZE = 32
 DISPLAY_COLS = 6
+CONFIDENCE_LEVEL = 0.8
 
 AUTOTUNE = tf.data.experimental.AUTOTUNE
 
-# Stone classifier model wrapper
-class BaseStoneClassifier:
+# Board elements classifier model wrapper
+class BoardItemClassifier:
     """This class wraps around TF model
     A stone images dataset made by cc/cc_gen.py is required for model training and prediction
     """
-    def __init__(self, model_dir, img_dir, img_size = (IMG_WIDTH, IMG_HEIGHT)):
+    def __init__(self, model_dir, img_dir, img_size = (IMG_WIDTH, IMG_HEIGHT), log_dir = None):
         """Constructor.
         Parameters:
             model_dir   Directory where a model is saved
@@ -38,7 +39,7 @@ class BaseStoneClassifier:
             img_size    Target image size
         """
         self.model = None
-        self.model_dir, self.img_dir, self.img_size = model_dir, img_dir, img_size
+        self.model_dir, self.img_dir, self.img_size, self.log_dir = model_dir, img_dir, img_size, log_dir
 
         self.image_data_gen = None
         self.train_generator = None
@@ -63,19 +64,26 @@ class BaseStoneClassifier:
         """Build new model"""
         print("==> Building model")
         self.model = tf.keras.models.Sequential([
-            tf.keras.layers.Conv2D(16, 3, padding='same', activation='relu', input_shape=(IMG_HEIGHT, IMG_WIDTH, 3)),
+            tf.keras.layers.Conv2D(16, 3, padding='same', activation='relu',
+                input_shape=(IMG_HEIGHT, IMG_WIDTH, 3),
+                kernel_regularizer=tf.keras.regularizers.l2(0.001)),
             tf.keras.layers.MaxPooling2D((2, 2)),
-            tf.keras.layers.Conv2D(32, 3, padding='same', activation='relu'),
+            tf.keras.layers.Dropout(0.1),
+            tf.keras.layers.Conv2D(32, 3, padding='same', activation='relu',
+                kernel_regularizer=tf.keras.regularizers.l2(0.001)),
             tf.keras.layers.MaxPooling2D((2, 2)),
-            tf.keras.layers.Conv2D(64, 3, padding='same', activation='relu'),
+            tf.keras.layers.Dropout(0.1),
+            tf.keras.layers.Conv2D(64, 3, padding='same', activation='relu',
+                kernel_regularizer=tf.keras.regularizers.l2(0.001)),
             tf.keras.layers.MaxPooling2D((2, 2)),
+            tf.keras.layers.Dropout(0.1),
             tf.keras.layers.Flatten(),
             tf.keras.layers.Dense(512, activation='relu'),
-            tf.keras.layers.Dense(1, activation='sigmoid')
+            tf.keras.layers.Dense(len(self.class_names), activation='softmax')
         ])
         self.model.compile(optimizer='adam',
-                           loss='binary_crossentropy',
-                           metrics=['accuracy'])
+                           loss='sparse_categorical_crossentropy',
+                           metrics=['sparse_categorical_accuracy'])
 
     def save(self):
         """Save whole model to specified directory"""
@@ -84,15 +92,20 @@ class BaseStoneClassifier:
     def init_datasets(self, display_samples = False):
         """Initialize datasets for training"""
         print("==> Loading images")
-        self.image_data_gen = ImageDataGenerator(rescale=1./255,
-                                            validation_split=0.2)
+        self.image_data_gen = ImageDataGenerator(
+            rescale=1./255,
+            #rotation_range=30,
+            #width_shift_range=.15,
+            #height_shift_range=.15,
+            #zoom_range=0.5,
+            validation_split=0.2)
 
         self.train_generator = self.image_data_gen.flow_from_directory(
             batch_size=BATCH_SIZE,
             directory=self.img_dir,
             shuffle=True,
             target_size=self.img_size,
-            class_mode='binary',
+            class_mode='sparse',
             subset='training')
 
         self.val_generator = self.image_data_gen.flow_from_directory(
@@ -100,7 +113,7 @@ class BaseStoneClassifier:
             directory=self.img_dir,
             shuffle=True,
             target_size=self.img_size,
-            class_mode='binary',
+            class_mode='sparse',
             subset='validation')
 
         if display_samples:
@@ -114,12 +127,17 @@ class BaseStoneClassifier:
         if self.train_generator is None:
             self.init_datasets()
 
+        callbacks = []
+        if self.log_dir is not None:
+            callbacks.extend([tf.keras.callbacks.TensorBoard(self.log_dir)])
+
         self.history = self.model.fit_generator(
                   self.train_generator,
                   epochs=epochs,
                   steps_per_epoch=self.train_generator.samples // BATCH_SIZE,
                   validation_data=self.val_generator,
-                  validation_steps=self.val_generator.samples // BATCH_SIZE)
+                  validation_steps=self.val_generator.samples // BATCH_SIZE,
+                  callbacks = callbacks)
 
         if display_history:
             self.display_history()
@@ -154,7 +172,8 @@ class BaseStoneClassifier:
         for n, d in enumerate(Path(self.img_dir).glob('*')):
             names = [str(f) for f in Path(self.img_dir).joinpath(d).glob('*.png')]
             file_names.extend(names)
-            file_labels.extend([1.0 * n] * len(names))
+            labels = [float(x == d.name) for x in self.class_names]
+            file_labels.extend([labels] * len(names))
 
         random_file_names = []
         random_file_labels = []
@@ -195,8 +214,8 @@ class BaseStoneClassifier:
         if self.history is None:
             return
 
-        acc = self.history.history['accuracy']
-        val_acc = self.history.history['val_accuracy']
+        acc = self.history.history['sparse_categorical_accuracy']
+        val_acc = self.history.history['val_sparse_categorical_accuracy']
 
         loss = self.history.history['loss']
         val_loss = self.history.history['val_loss']
@@ -229,10 +248,18 @@ class BaseStoneClassifier:
             num_rows = int(np.ceil(elements[0].shape[0] / DISPLAY_COLS))
             n_elem = 1
 
-            for image, true_label in zip(elements[0], elements[1]):
+            for i, e in enumerate(elements[0]):
+                g = tf.expand_dims(e, 0)
+                x = classifier.model.predict_classes(g)
+                print("{}: {}".format(i, x))
+
+            for image, labels in zip(elements[0], elements[1]):
+                true_label = int(np.argmax(labels))
                 try:
                     prediction = next(pred_iter)
-                    pred_label = int(np.round(prediction, 0))
+                    pred_label = int(np.argmax(prediction))
+                    if prediction[pred_label] < CONFIDENCE_LEVEL:
+                        pred_label = -1
                 except StopIteration:
                     break
 
@@ -244,25 +271,32 @@ class BaseStoneClassifier:
                 plt.imshow(image, cmap=plt.cm.binary)
 
                 plt.title('{} ({})'.format(
-                    self.class_names[pred_label],
-                    self.class_names[int(true_label)]))
+                    self.class_names[pred_label] if pred_label >= 0 else 'none',
+                    self.class_names[true_label]))
 
             plt.tight_layout()
 
         plt.show()
 
-def main():
-    img_dir = str(Path(__file__).absolute().parent.parent.joinpath('cc', 'p'))
-    model_dir =  str(Path(__file__).absolute().parent.joinpath('stone_classifier'))
+classifier = None
 
-    classifier = BaseStoneClassifier(model_dir, img_dir)
+def main():
+    global classifier
+
+    img_dir = str(Path(__file__).absolute().parent.parent.joinpath('cc', 'crossings'))
+    model_dir =  str(Path(__file__).absolute().parent.joinpath('crossing_classifier'))
+    log_dir =  str(Path(__file__).absolute().parent.joinpath('_logs'))
+
+    classifier = BoardItemClassifier(model_dir, img_dir, log_dir = log_dir)
     if classifier.exists():
         classifier.load()
     else:
-        classifier.train(display_history = True)
+        classifier.init_datasets(display_samples=True)
+        classifier.train()
         classifier.save()
+        classifier.display_history()
 
-    classifier.predict(32)
+    classifier.predict(64)
 
 if __name__ == '__main__':
     main()
